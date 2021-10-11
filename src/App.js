f86d6cc52b9b4d77a1ed589211b8472cf85a4e0c
Juan Galarce <j6alarce@gmail.com>
2021-10-11 15:34:54 -0300

    Se crea el component Card y Container para gestionar el diseño de nuestra app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import useForm from "./hooks/useForm";
 import Input from "./components/Input";
+import Card from "./components/Card";
+import Container from "./components/Container";
 
 const App = () => {
   const [form, handleChange] = useForm({ name: "", lastname: "" });
@@ -9,22 +11,28 @@ const App = () => {
 
   console.log(form);
   return (
-    <div>
-      <form>
-        <Input
-          label='Nombre'
-          name='name'
-          value={name}
-          onChange={handleChange}
-        />
-        <Input
-          label='Apellido'
-          name='lastname'
-          value={lastname}
-          onChange={handleChange}
-        />
-      </form>
-    </div>
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              autoComplete='off'
+              label='Nombre'
+              name='name'
+              value={name}
+              onChange={handleChange}
+            />
+            <Input
+              autoComplete='off'
+              label='Apellido'
+              name='lastname'
+              value={lastname}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 };
 
